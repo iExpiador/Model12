39bcf4f149cdd370523982f574fad8d439a3fb44
iExpiador <viruswebmail@gmail.com>
2025-09-13 19:38:50 -0500

    Update musica1.js

--- musica1.js
+++ musica1.js
@@ -13,7 +13,7 @@
       });
 
       var audio = new Audio();
-      var source = 'creep.mp3';
+      var source = 'malo.mp3';
   
       audio.volume = 0.5;
       audio.autoplay = true;
@@ -41,4 +41,5 @@
       })
 
 })();
+
 
